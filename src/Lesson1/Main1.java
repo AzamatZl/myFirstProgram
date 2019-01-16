26b0253b44a61190f583b1719e8744e529c28a94
AzamatZl <aktemirov04@mail.ru>
2019-01-16 20:32:37 +0300

    первый коммит первых 12 занятий

---
+++ src/Lesson1/Main1.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int rsr = 6;
+        int rtr = 15;
+        int ryr = 2;
+        int rir = 3;
+
+        int colvo = (rsr*rtr) / (ryr*rir);
+        System.out.println(colvo);
+    }
+}
---
+++ src/Lesson1/Main2.java
@@ -0,0 +1,22 @@
+package Lesson1;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int summa = 1000;
+        int igrushka = 175;
+        int gevacka = 11;
+        int confeta = 2;
+
+        double numIgr = summa / igrushka;  // кол-во игрушек
+      //  int ost = (numIgr * igrushka) - summa;
+        double sum1 = summa - numIgr * igrushka;
+        double numGev = sum1 / gevacka; // кол-во жевачек
+        double sum2 = sum1 - numGev * gevacka;
+        double numConf = sum2 / confeta;
+        double sum3 = s
+        System.out.println("Игрушек " + numIgr);
+        System.out.println("Жевачки " + numGev);
+
+
+    }
+}
